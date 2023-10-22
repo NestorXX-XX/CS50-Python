12955c9a31e67387c8f7fe7dff4933900629990a
NestorXX-XX <143557627+NestorXX-XX@users.noreply.github.com>
2023-10-22 19:10:58 +0100

    PATH.py

--- Course/Final Project/PATH.py
+++ Course/Final Project/PATH.py
@@ -1,3 +1,3 @@
 def path():
     ## WRITE HERE YOUR PATH TO THE DATA BASE FOLDER, DO NOT CHANGE ANY FOLDER OR FILE NAME
-    return "/Users/nestorxx-xx/Library/Mobile Documents/com~apple~CloudDocs/CODE/Visual Studio/Python/CS50/143557627/CS50_Python/Final_Project/Data_base"
+    return "write your path"
